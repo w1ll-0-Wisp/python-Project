b34762c123962f2f393078a0c77a0889a10e350b
w1ll-0-Wisp <tracer264@gmail.com>
2025-12-08 21:58:45 -0500

    added a message for you Professor
    added a little message

--- rolls.py
+++ rolls.py
@@ -1,6 +1,13 @@
 import random
 import argparse
 
+
+#hello sir, here is my project! ive been practicing argparse for a bit, on my pc so that why you dont see it in the github account, these practices were not for this project but for personal use
+#this was very fun to do, i watched a few videos for argparsing and what you can do with random! thank you for the great semester and hope that we can speak again in the future!
+#script by Eric Carriere 000906679 
+# ^w^
+
+
 Parser=argparse.ArgumentParser(
     description= "this is a D20 dice roll a D20 is a 20 sided dice, you can change the max dice face from 20 to anything, the script will roll a dice and export it to a .txt file"
 )
@@ -28,4 +35,5 @@ try:
 
          print(f"The file {file_path} was written succesfully go look at your roll!")
 except FileExistsError:
+
     print("this file already exists")
